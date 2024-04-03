eb15e6a07f6be7f4a378d87cff0e066e07b74cb1
AKHIL SINGH CHAUHAN <60491139+akhilub@users.noreply.github.com>
2024-04-03 10:46:50 -0400

    LC_226

---
+++ BinaryTree/LC_226_Invert_a_Binary_Tree.py
@@ -0,0 +1,25 @@
+# Approach : Using Recursion to invert a Binary tree
+# The terminal case is to invert a None node which is None
+# Now just recursively invert left and right subtrees and assign it to right and left pointers
+
+#TC:O(N) SC:O(N)
+# where N is the no of nodes in the binary Tree
+
+#Defination of binary Tree
+#class TreeNode:
+#   def __init__(self,val =0,left=None,right=None):
+#       self.val = val
+#       self.left = left
+#       self.right =right
+
+class Solution:
+    def invertTree(self,root):
+        if root is None:
+            return None
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left = right
+        root.right = left
+        return root
+
+
