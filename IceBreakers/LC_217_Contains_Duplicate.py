7105d5da42e61819aa1897e69fbde4c30bfe3050
AKHIL SINGH CHAUHAN <60491139+akhilub@users.noreply.github.com>
2024-04-07 13:50:18 -0400

    LC_217

---
+++ IceBreakers/LC_217_Contains_Duplicate.py
@@ -0,0 +1,40 @@
+#Approach: Use a HashSet
+#TC: O(N) ,where N is in the number of elements in the input array
+#SC: O(N)
+
+
+class Solution:
+    def containsDuplicate(self,nums):
+        seen  = set()
+        for ele in nums:
+            if ele in seen:
+                return True
+            seen.add(ele)
+        return False
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums1 = [1,2,3,4]
+    print(sol.containsDuplicate(nums1)) #Expected Output : False
+
+    sol = Solution()
+    nums2 = [1,2,3,1]
+    print(sol.containsDuplicate(nums2)) #Expected Output : True
+
+    sol = Solution()
+    nums3 = [1,1,1,3,3,4,3,2,4,2]
+    print(sol.containsDuplicate(nums3)) #Expected Output : False
+
+
+#Approach2 : Sorting can help
+#TC: O(NLogN)
+#SC: 0(1) or O(N)
+class Solution:
+    def containsDuplicate(self,nums):
+        nums.sort()
+        for idx, ele in enumerate(nums[:-1]):
+            if ele == nums[idx+1]:
+                return True
+        return False
+        
+        
