5080dbcd0e3405ca4e66d7add1fd82ffe6953340
AKHIL SINGH CHAUHAN <60491139+akhilub@users.noreply.github.com>
2024-04-10 18:58:13 -0400

    Added Comments of understanding on LC_84

--- Miscellaneous/LC_84_Largest_Rectangle_in_Histogram.py
+++ Miscellaneous/LC_84_Largest_Rectangle_in_Histogram.py
@@ -1,18 +1,17 @@
 class Solution:
-    def largestRectangleArea(self,heights):
+    def largestRectangleArea(self, heights: List[int]) -> int:
         maxArea = 0
-        stack = [] #pair: index,height
-
-        for i,h in enumerate(heights):
+        stack = [] #pair:(startIndex,height)
+        
+        for i , h in enumerate(heights):
             startIndex = i
-            while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                maxArea = max(maxArea, height*(i-index))
-                startIndex =index
+            while stack and stack[-1][1] > h: # stack is not empty and top element in stack has a height less than the current height
+                index,height = stack.pop()  # pop the top elements from the top of stack to check if this height could be the maxArea of the rectangle
+                maxArea = max(maxArea,height*(i-index)) #width = currentIndex - index at which this height started i.e popped element index
+                startIndex = index # Extend the startIndex all the way backward to the index that we just popped
             stack.append((startIndex,h))
         
-
-        for i,h in stack:
-            maxArea = max(maxArea,h*(len(heights)-i))
-        
+        #Extend the enteries left in stack to all the way to the end of histogram
+        for i, h in stack:
+            maxArea = max(maxArea, h*(len(heights) - i ))
         return maxArea
