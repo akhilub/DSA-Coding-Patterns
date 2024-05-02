042a3e16dc9a5dda6a224510311d15d7282f7782
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-05-02 02:04:05 -0400

    LC_881

---
+++ Pattern:Greedy_Algorithm/Concept_1_LC_881_Boats_To_Save_People/LC_881_Boats_To_Save_People.py
@@ -0,0 +1,25 @@
+#Approach : Greedy , Two Pointer + Sorting
+
+#1)Sort the people weight array
+#2)Intialize two pointer : Set two pointers l at the start lightest ans r at the end heaviest
+#3)Select Optimal Pairs: Iterate through the array and pair the lighest person with the heaviest person if their comnbined weight is within the limit
+#4)Count Boats: For each iteration , incremenet the boat count .Move the r pointer towards the left 
+#5)Repeat Unit All are Accounted For:Count until all people are accounted for i.e until l <r
+#6) If i ==j increment the boat count by 1 to arrange for the last remaing person
+
+class Solution:
+    def numRescueBoats(self,people):
+        people.sort()
+        l , r = 0 , len(people) - 1 
+        ans = 0
+        while l < r :
+            if people[l] + people [ r] < = limit:
+                l+=1
+                r-=1
+            r-=1
+            ans+=1
+
+            if l == r:
+                ans +=1
+
+            return ans
---
+++ Pattern:Greedy_Algorithm/GreedyNotes.md
@@ -0,0 +1,11 @@
+- Greedy algorithm is used to solve problems in which the best choice is made at each step and it finds a solution at minimal step.This approach assumes that choosing a local optimum at each stage lead to will lead to determination of global optimum.
+
+# Greedy Process
+1. Start with an empty solution set
+2. Iterative Item Selection 
+3. Add item to Solution Set 
+4. Feasibility Check
+5. Accept or reject the item
+6. Repeat Until Complete
+7. Assess Final Solution
+
