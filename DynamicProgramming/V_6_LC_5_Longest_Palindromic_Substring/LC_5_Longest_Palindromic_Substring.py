94ef632d74e1b035698cef8ced7308e8e37a3909
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-06-15 16:01:35 -0400

    Updated LC_5

--- DynamicProgramming/V_6_LC_5_Longest_Palindromic_Substring/LC_5_Longest_Palindromic_Substring.py
+++ DynamicProgramming/V_6_LC_5_Longest_Palindromic_Substring/LC_5_Longest_Palindromic_Substring.py
@@ -68,7 +68,7 @@ class Solution:
         for i in range(n):                                  #i is the left pointer 
             for j in range(n - 1, i - 1, -1):               #j is the right pointer 
                 if f(i, j):                                 #current string palindromic check function
-                    if j - i > R - L:                       #current length is greater than the longest length
+                    if j - i > R - L:                       #current length is greater than the longest length update the pointers for longest substring 
                         L = i
                         R = j
                     break
@@ -79,23 +79,23 @@ class Solution:
 
 #Write this in interviews
 class Solution:
-     def longestPalindrome(self, s: str) -> str:
-
+    def longestPalindrome(self, s: str) -> str:
         n = len(s)
         F = [[True]*n for _ in range(n)]
-
-        for l in range(n-2,-1,-1):
-            for r in range(l+1,n):
+        for l in range(n-2,-1,-1):                          #filling the grid from bottom up
+            for r in range(n-1,l,-1):
                 F[l][r] = s[l]==s[r] and F[l+1][r-1]
-
-        L=R=0
-        for i in range(n):
-            for j in range(n-1,-1,-1):
-                if F[i][j]:
-                    if j-i>R-L:
-                        L=i
-                        R=j
+        
+        L = 0
+        R = 0                                               #left and right index of longest palindrome
+        for i in range(n):                                  #i is the left pointer 
+            for j in range(n - 1, i - 1, -1):               #j is the right pointer 
+                if F[i][j]:                                 #current string palindromic check function
+                    if j - i > R - L:                       #current length is greater than the longest length update the longest substring pointers
+                        L = i
+                        R = j
                     break
         return s[L:R+1]
+        
 
 # The time/space complexity is O(N^2) quadratic.
