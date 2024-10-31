b6b10d010a84d8ccb2f82f6150b517dce44fc17b
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-10-31 15:57:05 +0530

    Updated Problems Algo

--- Graph/C_2_LC_261_Graph_Valid_Tree/LC_261_Graph_Valid_Tree.md
+++ Graph/C_2_LC_261_Graph_Valid_Tree/LC_261_Graph_Valid_Tree.md
@@ -1,5 +1,7 @@
 **[PlayGround](https://leetcode.com/problems/graph-valid-tree/)**
 
+**[PlayGround](https://www.lintcode.com/problem/178/)**
+
 You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [aᵢ, bᵢ] indicates that there is an undirected edge between nodes aᵢ and bᵢ in the graph.
 
 Return true if the edges of the given graph make up a valid tree, and false otherwise.
--- Stack/C_1_LC_20_Valid_Parentheses/LC_20_Valid_Parentheses.py
+++ Stack/C_1_LC_20_Valid_Parentheses/LC_20_Valid_Parentheses.py
@@ -1,3 +1,94 @@
+#Approach:Stack
+
+#Write this in interviews
+
+'''
+To solve this problem, we can use a stack. We'll iterate through each character in the string. 
+
+If we encounter an opening bracket, we'll push it onto the stack. 
+If we encounter a closing bracket,
+    •we'll check if the stack is empty or 
+    •if the top of the stack and the poped top element from the stack doesn't match the corresponding opening bracket. 
+    
+If either condition is true, we return false.
+
+After processing all characters, we check if the stack is empty. If it is, all brackets were properly closed, and we return true. 
+If not, there are unmatched opening brackets, so we return false.
+
+The time complexity of this solution is O(n), where n is the length of the input string. We iterate through each character once, and stack operations (push and pop) are O(1)
+'''
+
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []                                          # Stack to keep track of opening brackets
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            elif len(stack)==0 or (stack.pop()+ch) not in ["()","{}","[]"]:
+                return False
+            
+        return len(stack)==0
+
+
+
+
+
+
+
+
+
+
+"""
+len(stack)==0
+
+    ||
+equivalent
+    ||
+    
+not stack
+
+"""
+
+
+
+"""
+
+not (stack.pop() + ch in ["()","{}","[]"])
+
+                    ||
+                equivalent
+                    ||
+                    
+(stack.pop()+ch) not in ["()","{}","[]"]
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Approach 1: Using a stack
 
 '''
@@ -14,29 +105,18 @@ The time complexity is O(n), and the space complexity is O(n).
 Here, n is the length of the bracket string .
 '''
 
-class Solution:                                                            #class Solution:  
-    def isValid(self, s: str) -> bool:                                     #def isValid(self, s: str) -> bool:
-        stack = []                                                              #stack = []
-        for ch in s:                                                            #for ch in s:
-            if ch in '({[':                                                         #if ch in '({[':
-                stack.append(ch)                                                        #stack.append(ch)
-            elif len(stack)==0 or not (stack.pop() + ch in ["()","{}","[]"]):           # elif not stack or not (stack.pop() + ch in ["()","{}","[]"]):
-                return False                                                            #return False
-            return len(stack)==0                                                    # return not stack
-
-
-
-
-
-#Write this in interviews
-
-class Solution:
-    def isValid(self, s: str) -> bool:
+class Solution:                                                            
+    def isValid(self, s: str) -> bool:                                     
         stack = []
-        for ch in s:
-            if ch in '({[':
-                stack.append(ch)
-            elif len(stack)==0 or (stack.pop()+ch) not in ["()","{}","[]"]:
-                return False
-            
-        return len(stack)==0
+        valid = ["()","{}","[]"]
+                                                                      
+        for ch in s:                                                           
+            if ch in '({[':                                                        
+                stack.append(ch)                                                        
+            elif not stack or not (stack.pop() + ch in valid):          
+                return False                                                            
+        return not stack                                                 
+
+
+
+
